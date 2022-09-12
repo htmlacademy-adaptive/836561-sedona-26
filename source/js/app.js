443e87d3f912f16e38d0cb00660a54be2c88a586
Vetal <vitaliy792008@gmail.com>
2022-09-12 22:43:13 +0300

    Доработал сетку главной страницы

--- source/js/app.js
+++ source/js/app.js
@@ -1,8 +1,14 @@
-let navMain = document.querySelector('');
-let navToggle = document.querySelector('');
+let navMain = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.main-nav__toggle');
 
-navMain.classList.remove('');
+navMain.classList.remove('main-nav--nojs');
 
-navToggle.addEventListener('', function () {
-  if () {}
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
 })
